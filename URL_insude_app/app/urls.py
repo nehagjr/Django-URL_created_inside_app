a0c066e233ca02e2bc31dd513fae968a9fee46fa
nehagjr <pneha3169@gmail.com>
2024-06-27 18:35:15 +0530

    registration page created

--- URL_insude_app/app/urls.py
+++ URL_insude_app/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-# from .views import home
-from app import views
+from .views import home , register
+# from app import views
 
 urlpatterns = [
 #    path('home/',home),
-     path('home/',views.home)
+     path('home/',home),
+     path("registration/",register),
 ]
--- URL_insude_app/app/views.py
+++ URL_insude_app/app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+
 # Create your views here.
 
 def home(response):
-    return HttpResponse("<h1>Welcome</h1>")
+    # return HttpResponse("<h1 style='color:red'>Welcome</h1>")
+    data={
+        "name":"Neha",
+        "age":20,
+        "sub":"Django"
+    }
+    return JsonResponse(data)
+
+def register(request):
+    return render(request,'register.html')
