3c01d93b7cd4d868e27d864005deac8d57f0c12b
nehagjr <pneha3169@gmail.com>
2024-07-05 18:10:02 +0530

    template outside app

--- URL_insude_app/URL_insude_app/settings.py
+++ URL_insude_app/URL_insude_app/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
+TEMPLATES_DIR=os.path.join(BASE_DIR,"templates")
+print(TEMPLATES_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'URL_insude_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
